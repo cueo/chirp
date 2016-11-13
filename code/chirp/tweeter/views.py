d59de1a6709c9c4a46afda22e1f3c793ce1c66f6
doodhwala <mohit.surana95@gmail.com>
2016-11-14 00:22:25 +0530

    Rudimentary View

--- code/chirp/tweeter/views.py
+++ code/chirp/tweeter/views.py
@@ -8,10 +8,11 @@ def index(request):
     return HttpResponse("Chirp! This page is currently under development. Please come back later! :)")
 
 def user_profile(request, handle):
-    user = User.objects.get(handle=handle)
-    if(user):
+    try:
+        user = User.objects.get(handle=handle)
+    except Exception as e:
+        return HttpResponse("This user handle does not belong to anyone!")
+    else:
         tweets = Tweet.objects.all().filter(author=user)
         tweets = '<br/><br/>'.join([str(tweet) for tweet in tweets])
         return HttpResponse(tweets)
-    else:
-        return HttpResponse("This user handle does not belong to anyone!")
